0ce787ecaffe25f46e0de3d4b838a14630cc1401
kigundalevi <levimaxkigunda@gmai.com>
2025-07-01 23:52:53 +0300

    api url fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vHO7dB9luOvfJknah2X/tvL/FRhhiQyH4DPtEPTIE0U=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iXzViRhdELUagIzbWHy5Ts9I8y+g2bAyGoHjhJbB8m4=\"\n}"
